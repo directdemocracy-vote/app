fb08a9fd80610b9f4a90cf03edd75288b4c08bf1
megatol <74714212+megatol@users.noreply.github.com>
2023-07-26 09:33:44 +0200

    Create app.js

---
+++ httpdocs/js/app.js
@@ -0,0 +1,6 @@
+let app = new Framework7({el: '#app', name: 'directdemocracy', panel: {swipe: true}, routes: [{path: '/about/', url: 'about.html'}]});
+let mainView = app.views.create('.view-main');
+if (Framework7.device.ios)
+  console.log('It is iOS device');
+else if (Framework7.device.android)
+  console.log('It is Android device');
