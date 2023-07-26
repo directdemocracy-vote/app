19702380d1057b0377b90f3758e98337ecd6c429
megatol <74714212+megatol@users.noreply.github.com>
2023-07-26 15:48:49 +0200

    Update translator.js

--- httpdocs/js/translator.js
+++ httpdocs/js/translator.js
@@ -1,7 +1,23 @@
 "use strict"
+
 class Translator {
   constructor() {
-  
+    this._languages = ['en', 'fr'];
+    this._language = 'default';
+  }
+  set language(language) {
+    if (language === 'default')
+      this._language = navigator.languages ? navigator.languages[0] : navigator.language;
+    else
+      this._language = language;
+    if (!this._languages.includes(this._language))
+      this._language = this._language.substr(0, 2);
+    if (!this._languages.includes(this._language))
+      this._language = this._languages[0];
+    // load dictionnary
+  }
+  get language() {
+    return this._language;
   }
 }
 export default Translator;
