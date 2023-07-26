0bbaf7531c0e957accf7707691341b3c0fe4d75a
megatol <74714212+megatol@users.noreply.github.com>
2023-07-26 16:52:21 +0200

    Update app.js

--- httpdocs/js/app.js
+++ httpdocs/js/app.js
@@ -1,6 +1,7 @@
 import Translator from "./translator.js";
 let translator = new Translator();
 console.log(translator.language);
+console.log(translator._dictionary);
 
 let app = new Framework7({el: '#app', name: 'directdemocracy', panel: {swipe: true}, routes: [{path: '/about/', url: 'about.html'}]});
 let mainView = app.views.create('.view-main');
