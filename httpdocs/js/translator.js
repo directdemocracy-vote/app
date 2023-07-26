28d177050edcff74a540e5d976c122969d1b0cee
megatol <74714212+megatol@users.noreply.github.com>
2023-07-26 15:58:16 +0200

    Update translator.js

--- httpdocs/js/translator.js
+++ httpdocs/js/translator.js
@@ -3,7 +3,7 @@
 class Translator {
   constructor() {
     this._languages = ['en', 'fr'];
-    this._language = 'default';
+    this.language = 'default';
   }
   set language(language) {
     if (language === 'default')
