b545933cf2b323eb344453d17b1ee155aa55e754
megatol <74714212+megatol@users.noreply.github.com>
2023-07-28 16:37:17 +0200

    Update app.js

--- httpdocs/js/app.js
+++ httpdocs/js/app.js
@@ -2,7 +2,6 @@ import Translator from "https://directdemocracy.vote/js/translator.js";
 let translator = new Translator('/i18n');
 
 let app = new Framework7({el: '#app', name: 'directdemocracy', panel: {swipe: true}, routes: [{path: '/info/', pageName: 'info'}, {path: '/', pageName: 'home'}]});
-let mainView = app.views.create('.view-main');
 let languagePicker;
 app.on('pageInit', () => {
   console.log("Initialize language picker.");
@@ -18,3 +17,4 @@ app.on('pageBeforeRemove', () => {
   languagePicker.destroy();
 });
 
+let mainView = app.views.create('.view-main');
