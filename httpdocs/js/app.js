374efffbe9b5e83d474e96149ff0131c705a079c
megatol <74714212+megatol@users.noreply.github.com>
2023-07-28 16:55:31 +0200

    Update app.js

--- httpdocs/js/app.js
+++ httpdocs/js/app.js
@@ -4,6 +4,8 @@ let translator = new Translator('/i18n');
 let app = new Framework7({el: '#app', name: 'directdemocracy', panel: {swipe: true}, routes: [{path: '/info/', pageName: 'info'}, {path: '/', pageName: 'home'}]});
 let languagePicker;
 app.on('pageInit', function(page) {
+  if (page.name !== 'home')
+    return;
   console.log("Initialize language picker: " + page.name);
   languagePicker = app.picker.create({
     inputEl: '#language-picker',
