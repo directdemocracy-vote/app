17cd6b9ab9c93b519a6a7d2b3c0d68553e7ffec3
megatol <74714212+megatol@users.noreply.github.com>
2023-07-29 17:58:30 +0200

    Update app.js

--- httpdocs/js/app.js
+++ httpdocs/js/app.js
@@ -1,22 +1,28 @@
 import Translator from "https://directdemocracy.vote/js/translator.js";
+let languagePicker;
+let languages = [];
 let translator = new Translator('/i18n');
 
+translator.onready() {
+  for (let key in translator.languages) {
+    console.log(key, translator.languages[key]);
+    languages.push(translator.languages[key])
+  }
+  if (languagePicker)
+    languagePicker.setValue(languages);
+}
+
 let app = new Framework7({el: '#app', name: 'directdemocracy', panel: {swipe: true}, routes: [{path: '/info/', pageName: 'info'}, {path: '/', pageName: 'home'}]});
-let languagePicker;
+
+                   
 app.on('pageInit', function(page) {
   if (page.name !== 'home')
     return;
-  console.log("Initialize language picker: " + page.name);
-  console.log(translator);
-  console.log(translator.languages);
-  for (let key in translator.languages) {
-    console.log(key, translator.languages[key]);
-  }
   languagePicker = app.picker.create({
     inputEl: '#language-picker',
     cols: [{
       textAlign: 'center',
-      values: ['English', 'Français']
+      values: languages
     }]
   });
 });
