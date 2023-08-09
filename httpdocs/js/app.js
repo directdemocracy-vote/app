01403c632185e3920e3460acc3264d51b390ea78
megatol <74714212+megatol@users.noreply.github.com>
2023-08-09 09:50:04 +0200

    Update app.js

--- httpdocs/js/app.js
+++ httpdocs/js/app.js
@@ -1,4 +1,7 @@
-import Translator from "https://directdemocracy.vote/js/translator.js";
+import QrScanner from './qr-scanner.min.js';
+QrScanner.WORKER_PATH = 'js/qr-scanner-worker.min.js';
+
+import Translator from 'https://directdemocracy.vote/js/translator.js';
 let languagePicker;
 let homePageIsReady = false;
 let translatorIsReady = false;
@@ -51,3 +54,59 @@ app.on('pageBeforeRemove', function(page) {
 });
 
 let mainView = app.views.create('.view-main', {iosDynamicNavbar: false});
+
+window.onload = function() {
+  const DIRECTDEMOCRACY_VERSION = '0.0.2';
+  let citizen = {
+    schema: '',
+    key: '',
+    signature: '',
+    published: 0,
+    familyName: '',
+    givenNames: '',
+    picture: '',
+    latitude: 0,
+    longitude: 0
+  };
+  if (window.localStorage.getItem('registered'))
+    showPage('splash');
+  else
+    showPage('register');
+
+  // show either:
+  // 1. the register page when the citizen has not yet registered
+  // 2. the splash page when downloading registered citizen data
+  // 3. the card page once registered citizen data is available
+  function showPage(page) {
+    const pages = ['splash', 'register', 'card'];
+    if (!pages.includes(page)) {
+      console.error(`Page '${page}' not found`);
+      return;
+    }
+    document.getElementById(page + '-page').style.display = '';
+    pages.forEach(function(p) {
+      if (p !== page)
+        document.getElementById(p + '-page').style.display = 'none';
+    });
+    const cardItems = ['tabbar-endorse', 'tab-endorse', 'endorse', 'tabbar-vote', 'tab-vote', 'tabbar-sign', 'sign', 'edit', 'revoke-key'];
+    cardItems.forEach(function(i) {
+      if (page === 'card')
+        enable(i);
+      else
+        disable(i);
+    });
+  }
+  
+  function enable(item) {
+    let i = (typeof item === 'string') ? document.getElementById(item) : item;
+    i.classList.remove('disabled');
+  }
+
+  function disable(item) {
+    let i = (typeof item === 'string') ? document.getElementById(item) : item;
+    if (i.classList.contains('disabled'))
+      return;
+    i.classList.add('disabled');
+  }
+
+}
