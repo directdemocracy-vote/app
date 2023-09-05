c3ce22527b713f9acc4682f6fbeef9afa31f30ae
megatol <74714212+megatol@users.noreply.github.com>
2023-09-05 17:30:38 +0200

    better messages

--- httpdocs/app/js/app.js
+++ httpdocs/app/js/app.js
@@ -741,20 +741,20 @@ window.onload = function() {
         const outdated = (proposal.deadline < new Date().getTime());
         const deadline = new Date(proposal.deadline).toLocaleString();
         const title = `<b>${proposal.title}</b><br><br>`;
-        if (outdated) {
-          const message = (type === 'petition')
-                        ? `The deadline for signing this petition has passed. It was ${deadline}. Therefore you cannot sign it.`
-                        : `The deadline for voting at this referendum has passed. It was ${deadline}. Therefore you cannot vote.`;            
-          app.dialog.alert(`${title}${message}`, 'Deadline expired');
-        } else if (!proposal.inside) {
+        if (type === 'petition' && proposal.secret)
+          app.dialog.alert(`${title}This proposal is a referendum, not a petition, please scan it from the <b>Vote</b> tab`, 'Not a petition');
+        else if (type === 'referendum' && !proposal.secret)
+          app.dialog.alert(`${title}This proposal is a petition, not a referendum, please scan it from the <b>Sign</b> tab`, 'Not a referendum');
+        else if (!proposal.inside) {
           const message = (type === 'petition')
                         ? `You are not inside the area of this petition (which is <i>${proposal.areas[0].split('=')[1]}</i>). Therefore you cannot sign it.`
                         : `You are not inside the area of this referendum (which is <i>${proposal.areas[0].split('=')[1]}</i>). Therefore you cannot vote.`;
           app.dialog.alert(`${title}${message}`, 'Wrong area');
-        } else if (type === 'petition' && proposal.secret) {
-          app.dialog.alert(`${title}This proposal is a referendum, not a petition, please scan it from the <b>Vote</b> tab`, 'Not a petition');
-        } else if (type === 'referendum' && !proposal.secret) {
-          app.dialog.alert(`${title}This proposal is a petition, not a referendum, please scan it from the <b>Sign</b> tab`, 'Not a referendum');
+        } else if (outdated) {
+          const message = (type === 'petition')
+                        ? `The deadline for signing this petition has passed. It was ${deadline}. Therefore you cannot sign it.`
+                        : `The deadline for voting at this referendum has passed. It was ${deadline}. Therefore you cannot vote.`;            
+          app.dialog.alert(`${title}${message}`, 'Deadline expired');
         } else {
           let already = false;
           let proposals = (type === 'petition') ? petitions : referendums;
