b23a0dfd52e003233014ce8000078f40b69d5a14
megatol <74714212+megatol@users.noreply.github.com>
2023-09-12 11:59:36 +0200

    v

--- httpdocs/app/js/app.js
+++ httpdocs/app/js/app.js
@@ -898,6 +898,7 @@ window.onload = function() {
         label.appendChild(input);
         input.setAttribute('type', 'radio');
         input.setAttribute('name', `answer-${proposal.id}`);
+        input.setAttribute('value', answer);
         let i = document.createElement('i');
         i.classList.add('icon-radio', 'margin-right-half');
         label.appendChild(i);
