4b98c427f18d6bc228fd2015ffc50d033523c772
megatol <74714212+megatol@users.noreply.github.com>
2023-09-26 10:13:55 +0200

    t

--- httpdocs/app/js/app.js
+++ httpdocs/app/js/app.js
@@ -1143,6 +1143,7 @@ function updateCitizenCard() {
   let published = new Date(citizen.published * 1000);
   document.getElementById('citizen-published').innerHTML = published.toISOString().slice(0, 10);
   citizenFingerprint = CryptoJS.SHA1(atob(citizen.signature)).toString();
+  console.log("citizen fingerprint = " + citizenFingerprint);
   getReputationFromJudge();
   updateCitizenEndorsements();
 }
