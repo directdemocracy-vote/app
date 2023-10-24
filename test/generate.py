9ece68144b29c729857261b6d43abaf9add372c9
megatol <license@cyberbotics.com>
2023-10-24 10:45:32 +0200

    Generate keys and time stamp

--- test/generate.py
+++ test/generate.py
@@ -2,6 +2,9 @@ import base64
 import json
 import os
 import sys
+import time
+
+from Crypto.PublicKey import RSA
 
 for filename in os.listdir('citizen'):
     if filename.endswith('.json'):
@@ -12,6 +15,7 @@ for filename in os.listdir('citizen'):
             sys.exit('missing ' + citizen_file)
         with open(citizen_file, 'r', encoding='utf8') as file:
             citizen = json.load(file)
+        citizen['published'] = int(time.time())
         picture_file = os.path.join('picture', citizen_name + '.jpg')
         if not os.path.isfile(picture_file):
             sys.exit('missing ' + picture_file)
@@ -20,6 +24,13 @@ for filename in os.listdir('citizen'):
             sys.exit('Cannot open ' + picture_file)
         picture = file.read()
         citizen['picture'] = base64.b64encode(picture).decode('ascii')
+        key = RSA.generate(2048)
+        key_file = os.path.join('key', citizen_name + '.key')
+        with open(key_file, 'wb') as file:
+            os.chmod(key_file, 0o600)
+            file.write(key.exportKey('PEM'))
+        pubkey = key.publickey().exportKey('PEM')[27:-25].decode('ascii').replace("\n", '')
+        citizen['key'] = pubkey
         with open(citizen_file, 'w', encoding='utf8', newline='\n') as file:
             file.write(json.dumps(citizen, indent=4, ensure_ascii=False))
             file.write("\n")
