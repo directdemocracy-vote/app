36ee77286d1c33e2ebfe0c8035771d90cc9ae2af
megatol <74714212+megatol@users.noreply.github.com>
2023-10-29 09:46:29 +0100

    progress

--- httpdocs/api/integrity.php
+++ httpdocs/api/integrity.php
@@ -5,7 +5,15 @@ use Google\Client;
 use Google\Service\PlayIntegrity;
 use Google\Service\PlayIntegrity\DecodeIntegrityTokenRequest;
 
-$token = $_POST['token'];
+$citizen = json_decode(file_get_contents("php://input"));
+if (!$citizen)
+  die('Unable to parse JSON post');
+if (!isset($citizen->token))
+  die('Unable to read token');
+$token = $citizen->token;
+
+# if the integrity check is successful, this means the citizen blob is well formed because
+# it was created by a geniune app, so we don't need to check it
 $client = new Client();
 $client->setAuthConfig('../../directdemocracy.json');
 $client->addScope(PlayIntegrity::PLAYINTEGRITY);
@@ -14,8 +22,25 @@ $tokenRequest = new DecodeIntegrityTokenRequest();
 $tokenRequest->setIntegrityToken($token);
 $result = $service->v1->decodeIntegrityToken('vote.directdemocracy.app', $tokenRequest);        
 $verdict = $result->tokenPayloadExternal;
-$file = fopen("../../verdict.json", "w") or die("Unable to open file!");
+$file = fopen('../../verdict.json', 'w') or die('Unable to open verdict file!');
 fwrite($file, json_encode($verdict));
 fclose($file);
-die('{"result":"OK"}');
+if ($verdict->requestDetails->nonce !== $citizen->signature)
+  die('Wrong nonce');
+if ($verdict->requestDetails->packageName !== 'vote.directdemocracy.app')
+  die('Wrong package name');
+
+$public_key_file = fopen('../../test/id_rsa.pub', 'r') or die('Failed to read public key');
+$k = fread($public_key_file, filesize('../../test/id_rsa.pub'));
+fclose($public_key_file);
+if ($citizen->appKey !== stripped_key($k))
+  die('Wrong appKey');
+$private_key = openssl_get_privatekey('file://../../test/id_rsa');
+if ($private_key == FALSE)
+  die('Failed to read private key');
+$appSignature = '';
+$success = openssl_sign($citizen->signature, $appSignature, $private_key, OPENSSL_ALGO_SHA256);
+if ($success === FALSE)
+  die('Failed to sign citizen');
+die('{"appSignature":$appSignature}');
 ?>
