2fe79b0d3aae34cb8bcd502c3dbe6d41cfb5af51
Olivier Michel <Olivier.Michel@cyberbotics.com>
2023-12-01 16:32:29 +0100

    Update test.php

--- httpdocs/api/test.php
+++ httpdocs/api/test.php
@@ -1,5 +1,4 @@
 <?php
-$timezone = new DateTimeZone('Europe/Zurich');
-$offset = $timezone->getOffset(new DateTime) / 3600;
+$offset = new DateTimeZone('Europe/Zurich')->getOffset(new DateTime) / 3600;
 print($offset);
 ?>
