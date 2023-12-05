600a4d2603946861f4c965db6bf1d271f3ba06fe
Olivier Michel <Olivier.Michel@cyberbotics.com>
2023-12-05 11:16:07 +0100

    Update publish_participation.php

--- php/publish_participation.php
+++ php/publish_participation.php
@@ -1,3 +1,32 @@
 <?php
-print("hello world");
+require_once('database.php');
+
+print("hello world\n");
+$query = "SELECT * FROM participation WHERE published >= NOW()";
+$result = $mysqli->query($query) or die($mysqli->error);
+while ($row = $result->fetch_assoc()) {
+  $participation = array(
+    'schema' => "https://directdemocracy.vote/json-schema/$row[version]/participation.schema.json",
+    'key' => $row['key'],
+    'signature' => $row['signature'],
+    'published' => $row['published'], # FIXME
+    'appKey' => $row['appKey'],
+    'appSignature' => $row['appSignature'],
+    'referendum' => $row['referendum'],
+    'encryptedVote' => $row['encryptedVote']
+  );
+  $data = json_encode($participation, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+  $options = array('http' => array('method' => 'POST',
+                                   'content' => $data,
+                                   'header' => "Content-Type: application/json\r\n" .
+                                               "Accept: application/json\r\n"));
+  $response = file_get_contents("$notary/api/publish.php", false, stream_context_create($options));
+  $json = json_decode($response);
+  if (json_last_error() !== JSON_ERROR_NONE)
+    die($response);
+  if (isset($json->error))
+    die($json->error);
+  $mysqli->query("DELETE FROM participation WHERE id=$row[id]");
+}
+$result->free();
 ?>
