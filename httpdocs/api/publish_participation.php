dff6d48fbf0b85faa23578d873f23dab81e1d047
Olivier Michel <Olivier.Michel@cyberbotics.com>
2023-12-05 12:27:59 +0100

    Update publish_participation.php

--- httpdocs/api/publish_participation.php
+++ httpdocs/api/publish_participation.php
@@ -11,7 +11,14 @@ header("Access-Control-Allow-Headers: content-type");
 
 $notary = 'https://notary.directdemocracy.vote';
 
-$response = file_get_contents("$notary/api/publish.php");
+$data = json_encode("{\"test\":1}");
+$options = array('http' => array('method' => 'POST',
+                                 'content' => $data,
+                                 'ignore_errors' => true,
+                                 'user_agent' => "DirectDemocracy/$row[version]",
+                                 'header' => "Content-Type: application/json\r\n"
+                                            ."Accept: application/json\r\n"));
+$response = file_get_contents("$notary/api/publish.php", false, stream_context_create($options));
 die("{\"result\":\"$response\"}");
 
 $query = "SELECT * FROM participation WHERE published <= NOW()";
