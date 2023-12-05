89c663a2e76f95d6751a7976929e514515716276
Olivier Michel <Olivier.Michel@cyberbotics.com>
2023-12-05 22:19:12 +0100

    Update blind-sign.php

--- httpdocs/api/blind-sign.php
+++ httpdocs/api/blind-sign.php
@@ -35,7 +35,7 @@ function blind_verify($n, $e, $msg, $signature) {
   $modBits = strlen($n_bytes) * 8;
   $emLen = intval(ceil(($modBits - 1) / 8));
   $em = gmp_export($m, 1, GMP_BIG_ENDIAN | GMP_MSW_FIRST);
-  if (strcmp(bin2hex($em), $test_encoded_msg) !== 0)
+  if (strcmp($em, hex2bin($test_encoded_msg)) !== 0)
     die("wrong encoded message em<br><pre>".bin2hex($em)."</pre><br><pre>$test_encoded_msg</pre>");
   if (strlen($em) !== $emLen)
     die("emLen mismatch: ".strlen($em)." !== $emLen");
