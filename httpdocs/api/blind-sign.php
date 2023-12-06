d1d56086a2673c2f6596a1b1337e7ddbea1f5c06
Olivier Michel <Olivier.Michel@cyberbotics.com>
2023-12-06 12:04:22 +0100

    Update blind-sign.php

--- httpdocs/api/blind-sign.php
+++ httpdocs/api/blind-sign.php
@@ -56,8 +56,8 @@ function blind_verify($n, $e, $msg, $signature) {
     return "inconsistent rightmost octet";
   $maskedDB = substr($em, 0, $emLen - $hLen - 1);
   $H = substr($em, $emLen - $hLen - 1, $hLen);
-  print("maskedDB.length = ".strlen($maskedDB)." = $emLen - $hLen - 1");
-  $shift = 8 * $emLen - $modBits - 1;
+  print("maskedDB.length = ".strlen($maskedDB)." = $emLen - $hLen - 1\n");
+  $shift = 8 * $emLen - ($modBits - 1);
   print("shift = $shift\n");
   $mask = (0xff00 >> (8 * $emLen - $modBits - 1) & 0xff);
   if (ord(maskedDB[0]) & $mask != 0)
