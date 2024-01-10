353a2e91a142d66b04ab94c0834519f0eba2b01a
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-01-10 12:25:08 +0100

    Update response.php

--- httpdocs/api/response.php
+++ httpdocs/api/response.php
@@ -9,6 +9,14 @@ require_once '../../php/sanitizer.php';
 $id = intval($_GET['id']);
 $key = sanitize_field($_GET['key'], 'base64', 'key');
 $signature = sanitize_field($_GET['signature'], 'base64', 'signature');
+$file = "../../challenges/$id";
+$counter = 0;
+while (!file_exists($file)) {
+  usleep(250000); # wait for 0.25 seconds
+  $counter += 0.25;
+  if ($counter >= 60) # die after one minute
+    die('{"response":[]}');
+}
 require_once '../../php/database.php';
 $query = "SELECT REPLACE(REPLACE(TO_BASE64(`key`), '\\n', ''), '=', '') AS `key`, REPLACE(REPLACE(TO_BASE64(signature), '\\n', ''), '=', '') AS signature FROM response WHERE id=$id";
 $mysqli->query("LOCK TABLES response WRITE");
@@ -20,7 +28,7 @@ while($response = $r->fetch_assoc())
 $r->free();
 if ($answer) {
   $mysqli->query("DELETE FROM response WHERE challenge=$id") or die($mysqli->error);
-  unlink("../../challenges/$id");
+  unlink($file);
 }
 $mysqli->query("UNLOCK TABLES");
 $mysqli->close();
