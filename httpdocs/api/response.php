dc4f9b73277fbeb1c8a428128ac90eabe24d1af5
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-01-10 12:21:03 +0100

    Update response.php

--- httpdocs/api/response.php
+++ httpdocs/api/response.php
@@ -11,16 +11,18 @@ $key = sanitize_field($_GET['key'], 'base64', 'key');
 $signature = sanitize_field($_GET['signature'], 'base64', 'signature');
 require_once '../../php/database.php';
 $query = "SELECT REPLACE(REPLACE(TO_BASE64(`key`), '\\n', ''), '=', '') AS `key`, REPLACE(REPLACE(TO_BASE64(signature), '\\n', ''), '=', '') AS signature FROM response WHERE id=$id";
+$mysqli->query("LOCK TABLES response WRITE");
 $r = $mysqli->query($query) or die($mysqli->error);
-while($response = $r->fetch_assoc()) {
-}
-if ($challenge) {
-  $query = "INSERT INTO response(challenge, `key`, `signature`) VALUES($id, FROM_BASE64('$key=='), FROM_BASE64('$signature=='))";
-  $mysqli->query($query) or die($mysqli->error);
+$answer = [];
+$ids = '';
+while($response = $r->fetch_assoc())
+  $answer[] = $response;
+$r->free();
+if ($answer) {
+  $mysqli->query("DELETE FROM response WHERE challenge=$id") or die($mysqli->error);
   unlink("../../challenges/$id");
-  $mysqli->close();
-  die('{"key":"'.$challenge['key'].'","signature":"'.$challenge['signature'].'"}');
 }
+$mysqli->query("UNLOCK TABLES");
 $mysqli->close();
-die('{"error":"challenge not found"}');
+die('{"key":"'.$challenge['key'].'","signature":"'.$challenge['signature'].'"}');
 ?>
