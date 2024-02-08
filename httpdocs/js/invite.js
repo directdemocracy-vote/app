a7695ecade84b35a97758eca34eb91d82894fd6e
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-02-08 12:29:26 +0100

    Create invite.js

---
+++ httpdocs/js/invite.js
@@ -0,0 +1,22 @@
+function findGetParameter(parameterName) {
+  let result;
+  location.search.substr(1).split('&').forEach(function(item) {
+    const tmp = item.split('=');
+    if (tmp[0] === parameterName)
+      result = decodeURIComponent(tmp[1]);
+  });
+  return result;
+}
+
+window.onload = function() {
+  const fingerprint = findGetParameter('fingerprint');
+  fetch(`https://notary.directdemocracy.vote/api/publication.php?fingerprint=${fingerprint}`)
+    .then(response => response.json())
+    .then(answer => {
+      if (answer.error) {
+        console.error(answer.error);
+        return;
+      }
+      console.log(answer);
+    }
+}
