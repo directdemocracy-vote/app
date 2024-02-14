8f448a3a79f9598c2450e9a3c36e4d46e9843086
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-02-14 09:53:17 +0100

    Update translator.js

--- httpdocs/app/js/translator.js
+++ httpdocs/app/js/translator.js
@@ -58,11 +58,26 @@ class Translator {
     let elements = document.querySelectorAll('[data-i18n]');
     elements.forEach((element) => {
       const key = element.dataset.i18n;
+      let i = 1;
+      let parameter = [];
+      while (element.hasAttribute(`data-i18n-${i}`) {
+        parameter.push(element.getAttribute(`data-i18n-${i}`);
+        i++;
+      }
       if (element.nodeName === 'INPUT')
-        element.placeholder = this.translate(key);
+        element.placeholder = this.translate(key, parameter);
       else
-        element.innerHTML = this.translate(key);
+        element.innerHTML = this.translate(key, parameter);
     });
+  }
+  translateElement(element, key, parameter) {
+    element.setAttribute('data-i18n', key);
+    if (parameter instanceof Array) {
+      for (let i = 0; i < parameter.lenght; i++)
+        element.setAttribute(`data-i18n-${i + 1}`, parameter[i]);
+    } else if (parameter !== undefined)
+      element.setAttribute('data-i18n-1', parameter);
+    translate(key, parameter);
   }
   translate(key, parameter) {
     if (this.#dictionary === undefined)
@@ -70,6 +85,12 @@ class Translator {
     if (key in this.#dictionary) {
       if (parameter === undefined)
         return this.#dictionary[key];
+      if (paramerer instanceof Array) {
+        let translation = this.#dictionary['key'];
+        for (let i = 0; i < parameter.length; i++)
+          translation = translation.replace('%' + (i + 1), parameter[i]);
+        return translation;
+      }
       return this.#dictionary[key].replace('%1', parameter);
     } else {
       console.error(`Missing translation for key "${key}" in language "${this.language}".`);
