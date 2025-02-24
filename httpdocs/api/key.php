6d3489bc51628b995ce0b182865ea70887a6ff0a
Olivier Michel <Olivier.Michel@cyberbotics.com>
2025-02-24 20:59:51 +0100

    Update key.php

--- httpdocs/api/key.php
+++ httpdocs/api/key.php
@@ -6,9 +6,7 @@ function stripped_key($public_key) {
   $stripped = str_replace("\n", '', $stripped);
   return $stripped;
 }
-header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: content-type");
+require_once '../../php/header.php';
 $public_key_file = fopen("../../id_rsa.pub", "r") or die("{\"error\":\"unable to open public key file\"}");
 $k = fread($public_key_file, filesize("../../id_rsa.pub"));
 fclose($public_key_file);
