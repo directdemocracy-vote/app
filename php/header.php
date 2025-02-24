c7169975335a143eac0d639606d70c318a185fee
Olivier Michel <Olivier.Michel@cyberbotics.com>
2025-02-24 20:51:59 +0100

    Create header.php

---
+++ php/header.php
@@ -0,0 +1,6 @@
+<?php
+
+header("Content-Type: application/json");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, DirectDemocracy-Version, Integrity-Token, User-Notary, App-Time, Locality, Locality-Name, Latitude, Longitude');
