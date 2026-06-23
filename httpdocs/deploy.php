4987d3c39264ef888edc3f25f1bf5c1df3d905d6
Olivier Michel <Olivier.Michel@cyberbotics.com>
2026-06-23 18:05:12 +0200

    Enhance deploy.php to handle new parameter for pull

--- httpdocs/deploy.php
+++ httpdocs/deploy.php
@@ -1,5 +1,12 @@
 <?php
-print("<pre>$ git pull\n");
-print(shell_exec("git pull"));
+$new = isset($_GET['new']) && $_GET['new'] === '1';
+if ($new) {
+    $dir = realpath(__DIR__ . '/../../directdemocracy');
+    print("<pre>$ cd ../../directdemocracy && git pull\n");
+    print(shell_exec("git -C " . escapeshellarg($dir) . " pull"));
+} else {
+    print("<pre>$ git pull\n");
+    print(shell_exec("git pull"));
+}
 print("\n</pre>");
 ?>
