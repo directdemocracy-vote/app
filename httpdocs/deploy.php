2bc692077eded9565caa6d56edd4b7d0e6ad8fe5
Olivier Michel <Olivier.Michel@cyberbotics.com>
2026-07-27 13:12:15 +0200

    Simplify deploy script to always run git pull
    Removed conditional logic for deploying updates.

--- httpdocs/deploy.php
+++ httpdocs/deploy.php
@@ -1,12 +1,5 @@
 <?php
-$new = isset($_GET['new']) && $_GET['new'] === '1';
-if ($new) {
-    $dir = realpath(__DIR__ . '/../../directdemocracy');
-    print("<pre>$ cd ../../directdemocracy && git pull\n");
-    print(shell_exec("git -C " . escapeshellarg($dir) . " pull"));
-} else {
-    print("<pre>$ git pull\n");
-    print(shell_exec("git pull"));
-}
+print("<pre>$ git pull\n");
+print(shell_exec("git pull"));
 print("\n</pre>");
 ?>
